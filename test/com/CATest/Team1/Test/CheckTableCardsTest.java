3dd2b8fae11fccdd78547f4c628ce55bdb78eec8
MZN <MZN@MZN-HP>
2015-08-10 10:16:50 +0800

    Enhanced web ui showing log in in main page.

---
+++ test/com/CATest/Team1/Test/CheckTableCardsTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.CATest.Team1.Test;
+
+import com.CATest.Team1.Model.CardOnTable;
+import org.junit.*;
+
+
+public class CheckTableCardsTest {
+    
+    CardOnTable card= new CardOnTable();
+    
+    public CheckTableCardsTest() {
+        
+    }
+    @Test
+    public void getPossibleSetCart(){
+           
+        
+        
+    }
+    
+}
